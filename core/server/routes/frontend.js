7a0eb56fcebad9f184589e1130a834a6cba31015
Alex <theunknownartisthour@gmail.com>
2016-02-06 23:51:04 -0800

    Paypal IPN Route
    Adds a route /paypal/notification/ to handle instant payment notifications from paypal, uses the notification api to add messages about the order and buyer status.
    Can be used with user api to invite users with users api
    Also adds sandbox route /paypal/notification/sandbox/ for testing purposes

--- core/server/routes/frontend.js
+++ core/server/routes/frontend.js
@@ -57,6 +57,105 @@ frontendRoutes = function frontendRoutes(middleware) {
     router.get(/^\/((ghost-admin|admin|wp-admin|dashboard|signin|login)\/?)$/, function redirectToAdmin(req, res) {
         redirect301(res, subdir + '/ghost/');
     });
+    
+    	/*Example Expanded From: http://code.runnable.com/UtkFzblr0N9YAABV/paypal-ipn-listner-for-node-js-and-webserver */
+	router.post('/paypal/notification/:sandbox', function(req, res){
+		/*Test your configuration with https://developer.paypal.com/webapps/developer/applications/ipn_simulator */
+		
+		/*More variables from IPN here: https://developer.paypal.com/webapps/developer/docs/classic/ipn/integration-guide/IPNIntro/ */
+		console.log('Received POST /');
+		console.log(req.body);
+		console.log('\n\n');
+		
+		// assign posted variables to local variables
+		var item_name = req.body['item_name'];
+		var item_number = req.body['item_number'];
+		var payment_status = req.body['payment_status'];
+		var payment_amount = req.body['mc_gross'];
+		var payment_currency = req.body['mc_currency'];
+		var txn_id = req.body['txn_id'];
+		var receiver_email = req.body['receiver_email'];
+		var payer_email = req.body['payer_email'];
+		var payer_status = req.body['payer_status'];
+		var address_status = req.body['address_status'];
+		var first_name = req.body['first_name'];
+		var last_name = req.body['last_name'];
+		
+		var notetype = 'info';
+		var status = payment_status.toUpperCase();
+		
+		/*Customize as needed*/
+		if(status == "COMPLETED"){
+			notetype = 'success';
+		} 
+		else if(status == "DENIED"){
+			notetype = 'error';
+		} 
+		else if(status == "EXPIRED"){
+			notetype = 'warn';
+		} 
+		else if(status == "FAILED"){
+			notetype = 'error';
+		}
+		else if(status == "IN-PROGRESS"){
+			notetype = 'info';
+		}
+		else if(status == "PARTIALLY_REFUNDED"){
+			notetype = 'info';
+		}
+		else if(status == "PENDING"){
+			notetype = 'info';
+		}
+		else if(status == "PROCESSED"){
+			notetype = 'info';
+		}
+		else if(status == "REFUNDED"){
+			notetype = 'warn';
+		}
+		else if(status == "REVERSED"){
+			notetype = 'warn';
+		}
+		else if(status == "VOIDED"){
+			notetype = 'warn';
+		}
+		else if(status == "CANCELED_REVERSAL"){
+			notetype = 'warn';
+		} else {
+			notetype = 'info';
+		}
+		
+		/*Customized as needed*/
+		var usernote = 'info';
+		if(address_status.toUpperCase() == 'UNCONFIRMED'){
+			usernote = 'warn';
+		} else if(payer_status.toUpperCase() == 'UNVERIFIED'){
+			usernote = 'warn';
+		}
+		
+		/*Buyer Info Notification*/
+		api.notifications.add({notifications: [{
+			type: usernote,
+			message: first_name+' '+last_name+':'+payer_email+'<br>Address:'+address_status+'<br>Account:'+payer_status,
+		}]}, {context: {internal: true}});
+		
+		/*Purchase Info Notification*/
+		api.notifications.add({notifications: [{
+			type: notetype,
+			message: payer_email+'<br>'+item_name+' #'+item_number+'<br>'+payment_status+'<br>'+payment_amount+payment_currency,
+		}]}, {context: {internal: true}});
+		
+		/*
+		//Check if payment matches a specific item, invite the user by email
+		if(){
+			api.users.add();
+		}
+		*/
+		
+		// Spit back request as per IPN standards (DONE)
+		req.body = req.body || {};
+		res.send(200, 'OK');
+		res.end();
+	});
 
     // password-protected frontend route
     privateRouter.route('/')
